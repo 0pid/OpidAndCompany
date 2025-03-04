7440bca3808aa096451a81da556fe08ee4ba381c
0pid <mcopid@gmail.com>
2025-03-03 20:17:10 -0500

    V1.0.3
    Adjsuted orientation of the glowstick when it is on the ground
    Improved the holding position of the cute ceramic critter for a more natural look
    Adjusted the lighting effect of the glowstick
    Fixed spawn rate bug of cute ceramic critter item

---
+++ OpidAndCompany/OpidAndCompany/Behaviours/Glowstick.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpidAndCompany.Behaviours
+{
+    internal class Glowstick : PhysicsProp
+    {
+        public override void ItemActivate(bool used, bool buttonDown = true)
+        {
+            base.ItemActivate(used, buttonDown);
+            if (playerHeldBy != null)
+            {
+                //WIP
+            }
+        }
+    }
+}
